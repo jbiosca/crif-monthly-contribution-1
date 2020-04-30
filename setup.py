a91b511aaf0c359b4037d7510f51c058a08941ca
dcereijodo <dcereijo@digitalorigin.com>
2020-04-30 15:57:27 +0200

    fix setuptools config

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='crif-monthly-contribution',
     version='0.1',
-    py_modules=['target_crif'],
+    packages=find_packages(),
     install_requires=[
         'Click==7.1.2',
         'psycopg2==2.7.7',
         'pandas==1.0.3',
         'FixedWidth==1.3',
         'sqlalchemy-redshift==0.7.7',
-        'dbt==0.13.1'
+        'dbt==0.13.1',
+        'Logbook==1.5.3'
     ],
     entry_points='''
         [console_scripts]
-        cirf_monthly_contribution=target_crif:main
+        cirf_monthly_contribution=target_crif.target_crif:create_file
     ''',
 )
